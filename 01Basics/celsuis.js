ed9c209998863ea68a72fe5e823f6e7f978ca4fb
Akhil Binu <AkhilBinuRoom@gmail.com>
2023-08-21 16:26:32 +0530

    first commit

---
+++ 01Basics/celsuis.js
@@ -0,0 +1,20 @@
+var score = 100
+score = 130
+const bonus  = 50 
+
+var finalScore = (score + bonus ) * 2
+
+var complex = ((4 + 4) * 5) / 2
+
+
+// console.log(complex)
+
+// console.log(finalScore)
+
+// T(°C) = (T(°F) - 32) × 5/9
+
+let tempF = 140
+
+let celsius = (tempF - 32 ) * 5/9 
+
+console.log(celsius)
---
+++ 01Basics/variable.js
@@ -0,0 +1,24 @@
+let name = 'john'
+
+let score = 100
+let bonus = 20
+let totalscore = score + bonus ;
+
+console.log(totalscore);
+
+console.log(name);
+
+let firstName = 'Akhil '
+let lastName = 'Binu'
+
+console.log(firstName+lastName)
+
+console.log('Another Way')
+
+let fullName = firstName + lastName;
+
+console.log(fullName)
+
+const nam = 'akku'
+
+
